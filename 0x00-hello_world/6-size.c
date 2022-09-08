44bf779e4de0aadfc9422a6c9251ebdcad32c73b
mulalii <mundainronin@gmail.com>
2022-09-08 12:04:34 -0700

    kkkk

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.>
+/**
+* main - main block
+* Description: find size of data-types
+* return: 0
+*/
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(long int));
+	printf("%lu\n", sizeof(float));
+	printf("%lu", sizeof(long long int));
+	return (0);
+}
