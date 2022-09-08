6d120762048730bcd99c63b6acc7ba0f6bcc9ff1
mulalii <mundainronin@gmail.com>
2022-09-08 16:01:57 -0700

    lost

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,16 @@
 */
 int main(void)
 {
-	printf("%lu\n", sizeof(char));
-	printf("%lu\n", sizeof(int));
-	printf("%lu\n", sizeof(long int));
-	printf("%lu\n", sizeof(float));
-	printf("%lu", sizeof(long long int));
+	char c;
+	int i;
+	long li;
+	long long lli;
+	double d;
+	float f;
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
