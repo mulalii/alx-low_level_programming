b5ea9c912fc0682d9da1b58831def0e1028a7521
mulalii <mundainronin@gmail.com>
2022-09-08 10:52:35 -0700

    please

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-/*use puts to print message*/
-/*return 0*/
-
-int main(void) {
-
-	puts("\"Programming is like building a multilingual puzzle"); 
-
-		return 0 ;
-
+/**
+* main - main block
+*use puts to print message
+*return: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"); 
+return (0);
 }
-		
