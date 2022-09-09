9549370f03d7d726727f57b9e7c9cfa96aa393a8
mulalii <mundainronin@gmail.com>
2022-09-08 17:31:03 -0700

    nice

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,7 @@ int main(void)
 	int i;
 	long li;
 	long long lli;
-	float f;
-	
+	float f;	
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
