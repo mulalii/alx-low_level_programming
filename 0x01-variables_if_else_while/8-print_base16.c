87bf98e0e4a4f287ad8f8c66005251689208a7d6
mulalii <mundainronin@gmail.com>
2022-09-09 13:56:35 -0700

    gettt

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet at reverse
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
