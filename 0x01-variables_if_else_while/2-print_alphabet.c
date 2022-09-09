517fa9afc415e8aaf2a3bc9a925b0b7abe9216f8
mulalii <mundainronin@gmail.com>
2022-09-09 09:35:15 -0700

    a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print alphabet in order
+ * Return: 0;
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
