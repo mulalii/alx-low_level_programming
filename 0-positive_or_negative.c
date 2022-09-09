c5a56cb6c1bf7045bd589ceff23ca66bd0b018e9
mulalii <mundainronin@gmail.com>
2022-09-09 06:35:04 -0700

    decision

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: making code decision
+ * Return: 0;
+ */
+int main(void)
+{
+	int n = 10;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}
