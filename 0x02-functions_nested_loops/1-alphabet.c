dd21c4900e4a25219bcb94ba639701f60ca102f1
mulalii <mundainronin@gmail.com>
2022-09-13 06:12:47 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print alphabet
+ *
+ * Return: Always (success)
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while(a < 'z')
+	{
+		putchar(a);
+		a++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,10 @@
 #include <stdio.h>
+/**
+ * mainn - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	return (0);
+}
