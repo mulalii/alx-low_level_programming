dc6026af001d78eeff19010df8c524070736ecb6
mulalii <mundainronin@gmail.com>
2022-09-13 13:58:46 -0700

    later

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - check for lower case letter
+ * @c: c is a parameter is lower or uppercase
+ * Return: 0 or 1
+ */
+int_islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
