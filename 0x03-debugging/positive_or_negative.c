1e8897444f03c3c7a0363827db359ac1f13905aa
mulalii <mundainronin@gmail.com>
2022-09-14 16:45:14 -0700

    debugg

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,35 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+#include "main.h"
 /**
- * main - assign a random number to the variable n each time it is executed
- * and print the last digit of the number stored in the variable n
- * Return: Always (success)
+ * main - assigns a random number to int m everytime
+ * it executes, and prints it
+ * Return: Always 0 (success)
  */
-void positive_or_negative(int i))
+int positive_or_negative(int i)
 {
 	int n;
 
-	srand(time(0));
 	n = i;
 	if (n > 0)
 	printf("%d is positive\n", n);
-	if (n == 0)
+	else if (n == 0)
 	printf("%d is zero\n", n);
-	if (n < 0)
-{
+	else if (n < 0)
 	printf("%d is negative\n", n);
-}
-/**
- * main - debugging example
- * Return: 0
- */
-int main(void)
-{
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
 	return (0);
 }
