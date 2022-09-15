8e6eb3325f33711901c216c2fca681d0913c9b6e
mulalii <mundainronin@gmail.com>
2022-09-15 08:08:33 -0700

    0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: Always 0 (success0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = '0';
+	while (i < 10)
+	{
+		i++;
+	}
+	return (0);
+}
