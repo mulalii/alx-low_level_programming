be248b692b55c0f42f16c263183744ceb6a74a56
mulalii <mundainronin@gmail.com>
2022-09-19 08:22:54 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that resets an interger and updates value
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * swap_int - a code that swaps int value
+ * Return: Always (success)
+ */
+void swap_int(int *a, int *b)
+{
+	int *a = *b;
+	int *b = *a;
+}
