522ba691323893fbdca168a42db88fda5e56b862
mulalii <mundainronin@gmail.com>
2022-09-22 09:59:52 -0700

    run

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,11 @@
-include "main.h"
+#include "main.h"
 /**
- * _strncat - concatenate two stings
- * @dest - destination for string
- * @src - source to transfer
- * @n - limiter for string
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination for string
+ * @src: source to transfer
+ * @n: limiter for string
+ *
+ * Return: a pointer to the result desti
  */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -11,14 +13,15 @@ char *_strncat(char *dest, char *src, int n)
 	int q;
 
 	i = 0;
-	while (dest[i] != '\0'; dest[i] < n)
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	for (q = 0; src[q] != '\0')
+	for (q = 0; src[q] != '\0'; q++)
 	{
-		dest[i] = src[w];
+		q < n;
+		dest[i] = src[q];
 	}
 	dest = '\0';
-	return(0);
+	return (dest);
 }
