6b85f644079423c41b31d672313a4930a31c5cd4
mulalii <mundainronin@gmail.com>
2022-09-26 14:15:35 -0700

    better

---
+++ 0x07-pointers_arrays_strings/0-memst.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_memset - A function that fills memory with a constant byte
+ * @n: the size of the memory to print
+ * @s: pointer for n
+ * @b: constant byte 
+ * Return: success
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
+8}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - locate character in a string
+ * @s: sring variable
+ * @c: character variable
+ */
+char *_strchr(char *s, char c)
+{
+	int v;
+
+	for (v = 0; s[v] < '\0'; v++)
+	{
+		if (s[v] == n)
+		{
+			return(s + v);
+		}
+		return (0);
+	}
