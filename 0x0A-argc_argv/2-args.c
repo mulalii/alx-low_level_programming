2c450edc781fda495a2dccfa94bc29ba255443c9
mulalii <mundainronin@gmail.com>
2022-09-30 16:26:16 -0700

    release

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * @argc - number of arguments
+ * @argv - array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv)
+{
+	int i;
+
+	if (i == argc)
+	{
+		return(argc);
+	}
+	return(0);
+}
