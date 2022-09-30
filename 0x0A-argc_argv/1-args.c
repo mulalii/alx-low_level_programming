52331b466df85f5ba5aecddc5e380786c993711d
mulalii <mundainronin@gmail.com>
2022-09-30 16:39:04 -0700

    easy

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * @argc - number of arguments
+ * @argv - array of arguments
+ * Return: 0
+ */
+int main(int argc, char_attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return(0);
+}
