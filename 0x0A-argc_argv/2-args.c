3137893b3dd25737748bfdb1ec6f64b128e3bef3
mulalii <mundainronin@gmail.com>
2022-09-30 17:14:49 -0700

    please

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,18 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
-* main - prints its name follwoed by a newline
-* @argc: number of arguments
-* @argv: the vector that hold the arguments
-*
-* Description: prints the current directory and the name like ./name
-* Return: returns the name.
+* main - prints all arguments it received
+*@argc: argument count
+*@argv: argument vector
+*Return: returns 0
 */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
-	return (0);
+		int i;
+
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+
+		return (0);
 }
