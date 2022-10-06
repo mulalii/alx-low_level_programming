cc9fa338965306d40ee1c1af49a7afa041f67cba
mulalii <mundainronin@gmail.com>
2022-10-06 14:24:03 -0700

    uuuuuu

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <limits.h>
+/**
+ * malloc_checked - WAP that allocates memory using malloc
+ * @b: number of bytes to allocates
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return(ptr);
+}
